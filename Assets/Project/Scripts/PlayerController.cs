6c643ef990184b7b37f978722566a49ff728b475
Justin Wong <jusw85@hotmail.com>
2018-11-30 23:26:31 +0800

    Cleanup

--- Assets/Project/Scripts/PlayerController.cs
+++ Assets/Project/Scripts/PlayerController.cs
@@ -1,86 +1,72 @@
 using UnityEngine;
-using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed;
-    public float jumpHeight;
+    public enum DashState
+    {
+        Ready,
+        Dashing,
+        Cooldown
+    }
 
-    bool facingRight = true;
+    public enum DashUpState
+    {
+        Ready,
+        Dashing,
+        Cooldown
+    }
 
-    public Transform groundCheck;
-    public float groundCheckRadius;
-    public LayerMask whatIsGround;
+    private Animator anim;
+    private AudioManager audioManager;
 
-    private bool grounded;
-    private bool canDoubleJump;
-
-    public Transform buildPoint;
     public GameObject buildingBlock;
-
-    public Transform telePoint;
-    public GameObject teleBlock;
-
-    public float buildDelay;
-    private float buildDelayCounter;
-
-    public float boundDelay;
-    private float boundDelayCounter;
-
-    public Transform firePoint;
+    public Transform buildPoint;
     public GameObject bullet;
+    private bool canDoubleJump;
+    private bool cannotDash;
+    private bool cannotDashUp;
+    private bool facingRight = true;
 
-    public Transform firePoint_red;
-    public GameObject bullet_red;
-
-    public float knockback;
-    public float knockbackLength;
-    public float knockbackCount;
-    public bool knockFromRight;
-
-    public DashState dashState;
-    public float dashTimer;
-    public float maxDash = 10f;
-
-    public DashUpState dashUpState;
-    public float dashUpTimer;
-    public float maxDashUp = 10f;
-
+    [Space(10)] [Header("Dashing Stuff")] public GameObject dashBullet;
+    public Transform dashBulletPoint;
     public Transform dashCheck;
     public float dashCheckRadius;
-    public LayerMask whatIsWall;
-    private bool cannotDash;
-
+    public DashState dashState;
+    public float dashTimer;
     public Transform dashUpCheck;
     public float dashUpCheckRadius;
-    public LayerMask whatIsWallUp;
-    private bool cannotDashUp;
+    public DashUpState dashUpState;
+    public float dashUpTimer;
+    [Space(20)] public Transform firePoint;
+    public Transform groundCheck;
+    public float groundCheckRadius;
 
-    public Transform dashBulletPoint;
-    public GameObject dashBullet;
+    private bool grounded;
+    private bool isFacingRight = true;
+    public float jumpHeight;
 
-    public Transform boundPoint;
+    public float knockback;
+    public float knockbackCount;
+    public float knockbackLength;
+    public bool knockFromRight;
+    public float maxDash = 10f;
+    public float maxDashUp = 10f;
+    private Vector2 moveInput;
+    public float moveSpeed;
 
-    public GameObject phantomBound;
-    //private GameObject phantomBoundInstance;
-    //private bool isPhantomBoundActive = false;
-
-    public Sprite PlayerRed;
     public Sprite PlayerBlue;
+
+    private Rigidbody2D rb2d;
 
     private SpriteRenderer spriteRenderer;
 
-    private Rigidbody2D rb2d;
-    private Animator anim;
+//    public GameObject teleBlock;
+//    public Transform telePoint;
 
-    private float currentScale = 1.0f;
-    private Vector2 moveInput;
-    private bool isFacingRight = true;
-    private bool toJump = false;
-
-
-    //cache
-    private AudioManager audioManager;
+    private bool toJump;
+    public LayerMask whatIsGround;
+    public LayerMask whatIsWall;
+    public LayerMask whatIsWallUp;
 
     // Use this for initialization
     private void Awake()
@@ -89,14 +75,11 @@ public class PlayerController : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
-    void Start()
+    private void Start()
     {
         //caching
         audioManager = AudioManager.instance;
-        if (audioManager == null)
-        {
-            Debug.LogError("No audio Manager found");
-        }
+        if (audioManager == null) Debug.LogError("No audio Manager found");
 
         spriteRenderer = GetComponent<SpriteRenderer>();
 
@@ -113,7 +96,7 @@ public class PlayerController : MonoBehaviour
         if (grounded)
             canDoubleJump = true;
 
-        Vector2 newVelocity = rb2d.velocity;
+        var newVelocity = rb2d.velocity;
         newVelocity.x = moveInput.x * moveSpeed;
         if (toJump)
         {
@@ -125,17 +108,11 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         if (moveInput.x != 0)
             SetIsFacingRight(moveInput.x > 0);
-
-//        if (Input.GetKeyDown(KeyCode.E))
-//        {
-//            currentScale = 1.5f - currentScale;
-//            RescaleSprite(currentScale);
-//        }
 
         if (Input.GetButtonDown("Jump"))
         {
@@ -156,66 +133,24 @@ public class PlayerController : MonoBehaviour
         {
             audioManager.PlaySound("Firing");
             PlayerColorBullet();
-            anim.SetBool("Shooting", true);
-        }
-        else
-        {
-            anim.SetBool("Shooting", false);
         }
 
-        //This is to create phantom bound
-        if (Input.GetKeyDown(KeyCode.K) && Time.time > boundDelayCounter)
-        {
-            boundDelayCounter = Time.time + boundDelay;
-            Instantiate(phantomBound, boundPoint.position, boundPoint.rotation);
-        }
-
-
-        float moveVelocity = 0f;
-        if (Input.GetKey(KeyCode.D))
-        {
-            //GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
-
-            moveVelocity = moveSpeed;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            //GetComponent<Rigidbody2D> ().velocity = new Vector2 (-moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
-
-            moveVelocity = -moveSpeed;
-        }
-
-
-        if (knockbackCount <= 0)
-        {
-            rb2d.velocity = new Vector2(moveVelocity, rb2d.velocity.y);
-        }
-        else
-        {
-            if (knockFromRight)
-                transform.Translate(-0.1f, 0.1f, 0f);
-            if (!knockFromRight)
-                transform.Translate(0.1f, 0.1f, 0f);
-            knockbackCount -= Time.deltaTime;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            Instantiate(buildingBlock, buildPoint.position, buildPoint.rotation);
-            buildDelayCounter = buildDelay;
-        }
-
-        if (Input.GetKey(KeyCode.Return))
-        {
-            buildDelayCounter -= Time.deltaTime;
-
-            if (buildDelayCounter <= 0)
-            {
-                buildDelayCounter = buildDelay;
-                Instantiate(buildingBlock, buildPoint.position, buildPoint.rotation);
-            }
-        }
+//        var moveVelocity = 0f;
+//        if (Input.GetKey(KeyCode.D)) moveVelocity = moveSpeed;
+//        if (Input.GetKey(KeyCode.A)) moveVelocity = -moveSpeed;
+//
+//        if (knockbackCount <= 0)
+//        {
+//            rb2d.velocity = new Vector2(moveVelocity, rb2d.velocity.y);
+//        }
+//        else
+//        {
+//            if (knockFromRight)
+//                transform.Translate(-0.1f, 0.1f, 0f);
+//            if (!knockFromRight)
+//                transform.Translate(0.1f, 0.1f, 0f);
+//            knockbackCount -= Time.deltaTime;
+//        }
 
         //This is to teleport the character to the right
         switch (dashState)
@@ -290,14 +225,6 @@ public class PlayerController : MonoBehaviour
 
                 break;
         }
-
-        //this is to change Player from Red to blue to Red
-
-        if (Input.GetKeyDown(KeyCode.U))
-        {
-//            audioManager.PlaySound("Powerup");
-            PlayerColorChange();
-        }
     }
 
     private void LateUpdate()
@@ -311,7 +238,7 @@ public class PlayerController : MonoBehaviour
     {
         if (this.isFacingRight ^ isFacingRight)
         {
-            Vector3 scale = transform.localScale;
+            var scale = transform.localScale;
             scale.x *= -1;
             transform.localScale = scale;
         }
@@ -319,32 +246,9 @@ public class PlayerController : MonoBehaviour
         this.isFacingRight = isFacingRight;
     }
 
-    private void RescaleSprite(float newscale)
-    {
-        Vector3 scale = transform.localScale;
-        scale.x = Mathf.Sign(scale.x) * newscale;
-        scale.y = newscale;
-        transform.localScale = scale;
-    }
-
-    public enum DashState
-    {
-        Ready,
-        Dashing,
-        Cooldown
-    }
-
-    public enum DashUpState
-    {
-        Ready,
-        Dashing,
-        Cooldown
-    }
-
     private void dashRight()
     {
-        for (int i = 0; i < 5; i++)
-        {
+        for (var i = 0; i < 5; i++)
             if (isFacingRight)
             {
                 //GetComponent<Rigidbody2D> ().velocity = new Vector3 (2, 0, 0);
@@ -359,45 +263,19 @@ public class PlayerController : MonoBehaviour
                 Instantiate(buildingBlock, buildPoint.position, buildPoint.rotation);
                 Instantiate(dashBullet, dashBulletPoint.position, dashBulletPoint.rotation);
             }
-        }
     }
 
 
     private void dashUp()
     {
-        for (int i = 0; i < 5; i++)
-        {
+        for (var i = 0; i < 5; i++)
             transform.Translate(0f, 0.3f, 0f);
-            //Instantiate(teleBlock, telePoint.position, telePoint.rotation);
-        }
+        //Instantiate(teleBlock, telePoint.position, telePoint.rotation);
     }
 
-    void PlayerColorChange()
+    private void PlayerColorBullet()
     {
-        if (spriteRenderer.sprite == PlayerBlue)
-        {
-            spriteRenderer.sprite = PlayerRed;
-            gameObject.tag = "Player_red";
-        }
-        else
-        {
-            spriteRenderer.sprite = PlayerBlue;
-            gameObject.tag = "Player_blue";
-        }
-    }
-
-
-    void PlayerColorBullet()
-    {
-        if (gameObject.tag == "Player_blue")
-        {
-            audioManager.PlaySound ("Firing");
-            Instantiate(bullet, firePoint.position, firePoint.rotation);
-        }
-        else
-        {
-            audioManager.PlaySound ("Firing");
-            Instantiate(bullet_red, firePoint.position, firePoint.rotation);
-        }
+        audioManager.PlaySound("Firing");
+        Instantiate(bullet, firePoint.position, firePoint.rotation);
     }
 }
